7e3483fb07afe2bdc53f15de1c0d9b35c886ac6e
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2024-09-27 21:46:00 +0100

    initial commit
    Added a blank tvOS SwiftUI project with XCTest.

---
+++ Converter-SwiftUIVersion/Converter-SwiftUIVersion/Converter_SwiftUIVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Converter_SwiftUIVersionApp.swift
+//  Converter-SwiftUIVersion
+//
+//  Created by Steven Hill on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Converter_SwiftUIVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
