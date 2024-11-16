cbd88897dd0a61d0a6e27fe5dddc79129fff69a1
steven-hill <stevenhill15@icloud.com>
2024-11-16 12:35:50 +0000

    Title tests. Added a unit test for the TItleView. Added a UI test for the TitleView's text. Extracted TitleView to its own file in the newly created Views folder.

--- Converter-SwiftUIVersion/Converter-SwiftUIVersionUITests/Converter_SwiftUIVersionUITests.swift
+++ Converter-SwiftUIVersion/Converter-SwiftUIVersionUITests/Converter_SwiftUIVersionUITests.swift
@@ -10,34 +10,19 @@ import XCTest
 final class Converter_SwiftUIVersionUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func test_titleView_displaysCorrectText() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        let titleViewText = app.staticTexts["Measurement converter"]
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        XCTAssertTrue(titleViewText.exists, "The title text should exist.")
+        XCTAssertEqual(titleViewText.label, "Measurement converter", "The title text should be 'Measurement converter'.")
     }
 }
