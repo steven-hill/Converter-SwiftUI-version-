4a377cc8140dce9e783dfa4387e5d025f01cdc02
steven-hill <stevenhill15@icloud.com>
2024-11-19 21:17:08 +0000

    Add unit selection views. Added the UI tests for the two unit selection views, and added these views with their titles to ContentVIew().

---
+++ Converter-SwiftUIVersion/Converter-SwiftUIVersion/Views/UnitSelectionView.swift
@@ -0,0 +1,19 @@
+//
+//  UnitSelectionView.swift
+//  Converter-SwiftUIVersion
+//
+//  Created by Steven Hill on 19/11/2024.
+//
+
+import SwiftUI
+
+struct UnitSelectionView: View {
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .padding()
+        }
+    }
+}
